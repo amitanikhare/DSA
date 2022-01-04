2d1e297beb716ecdc73b75c9579bf6d86e8f4906
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-04 23:40:48 +0530

    Create AreaOfRectangle.java

---
+++ conditionals-loops/AreaOfRectangle.java
@@ -0,0 +1,13 @@
+//Area Of Rectangle
+import java.util.*;
+class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter width :- ");
+        float width = sc.nextFloat();
+        System.out.println("Enter height :- ");
+        float height = sc.nextFloat();
+        double area = (width * height);
+        System.out.println("Area Of Rectangle is :- "+area);
+        }
+    }
