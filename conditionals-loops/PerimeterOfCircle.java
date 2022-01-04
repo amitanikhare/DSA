31b3609de72f6a4a011867290f90d341a878ac10
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-05 00:41:31 +0530

    Create PerimeterOfCircle.java

---
+++ conditionals-loops/PerimeterOfCircle.java
@@ -0,0 +1,11 @@
+// Perimeter Of Circle
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter radius of circle :- ");
+        float r = sc.nextFloat();
+        double peri = 2*3.14*r;
+        System.out.println("Perimeter Of Circle :- "+peri);
+    }
+}
