59ad351571ab03331e9f326b81884f71c526338d
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-04 23:47:04 +0530

    Create AreaOfIsoscelesTriangle.java

---
+++ conditionals-loops/AreaOfIsoscelesTriangle.java
@@ -0,0 +1,13 @@
+//Area Of Isosceles Triangle
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter base :- ");
+        float base = sc.nextFloat();
+        System.out.println("Enter height :- ");
+        float height = sc.nextFloat();
+        double area = (1 * base * height)/2;
+        System.out.println("Area Of Rectangle is :- "+area);
+        }
+    }
