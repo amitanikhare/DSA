eb73faa1553ebb608016cd60be2103a7584ead1a
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-05 00:45:01 +0530

    Create PerimeterOfEquilateralTriangle.java

---
+++ conditionals-loops/PerimeterOfEquilateralTriangle.java
@@ -0,0 +1,11 @@
+//Perimeter Of Equilateral Triangle
+import java.util.*;
+class PerimeterOfEquilateralTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter sides Triangle:- ");
+        int a = sc.nextInt();
+        int peri = 3*a;
+        System.out.println("Perimeter Of Equilateral Triangle :- "+peri);
+    }
+}
