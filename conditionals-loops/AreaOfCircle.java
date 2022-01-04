1c3d54d27e782fab1e131cb569e0dd22c668a793
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-04 23:32:56 +0530

    Create AreaOfCircle.java

---
+++ conditionals-loops/AreaOfCircle.java
@@ -0,0 +1,12 @@
+//Area Of Circle
+import java.util.*;
+class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius :- ");
+        float r = sc.nextFloat();
+        
+        double area = 3.24*(r * r);
+        System.out.println("Area Of Circle is :- "+area);
+        }
+    }
