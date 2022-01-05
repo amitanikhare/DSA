c8c88271a0b56bd7b9769cf11ab368fcc5d9a2ca
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-05 19:06:52 +0530

    Create PerimeterOfRhombus.java

---
+++ conditionals-loops/PerimeterOfRhombus.java
@@ -0,0 +1,11 @@
+//Perimeter Of Rhombus
+import java.util.*;
+class PerimeterOfRhombus {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter sides :- ");
+        int a = in.nextInt();
+        int peri = 4*a;
+        System.out.println("Perimeter Of Rhombus :- "+peri);
+    }
+}
