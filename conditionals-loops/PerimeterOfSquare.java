dcbcf60c4d2311aa691866e939ee53797d068120
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-05 19:03:05 +0530

    Create PerimeterOfSquare.java

---
+++ conditionals-loops/PerimeterOfSquare.java
@@ -0,0 +1,11 @@
+//Perimeter Of Square
+import java.util.*;
+class PerimeterOfSquare {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter sides :- ");
+        int a = in.nextInt();
+        int peri = 4*a;
+        System.out.println("Perimeter Of Square :- "+peri);
+    }
+}
