454399e906513b6083b1a077539364778a56e37e
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-09 18:56:31 +0530

    Create Factorial.java

---
+++ conditionals-loops/Intermediate/Factorial.java
@@ -0,0 +1,10 @@
+//Factorial Program In Java.
+public class Factorial{
+	public static void main(String[] args) {
+		int fact=1 ,no = 4;
+		for(int i = 1 ; i<=no;i++){
+		    fact*=i;
+		}
+		System.out.println(no + " factorial is: "+fact);
+	}
+}
