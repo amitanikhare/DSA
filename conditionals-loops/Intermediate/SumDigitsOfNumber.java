b28651b949928fdb44a122b5bb90f169cf03ae12
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-12 20:34:50 +0530

    Create SumDigitsOfNumber.java

---
+++ conditionals-loops/Intermediate/SumDigitsOfNumber.java
@@ -0,0 +1,17 @@
+//Sum Of A Digits Of Number.
+import java.util.*;
+public class SumDigitsOfNumber{
+	public static void main(String[] args) {
+		System.out.println("Enter digits: ");
+		Scanner in = new Scanner(System.in);
+		int rem =0 , sum =0 , original;
+		int num = in.nextInt();
+		original = num;
+		while(num>0){
+		    rem = num % 10;
+		    sum += rem;
+		    num/=10;
+		}
+		System.out.println("sum : "+sum);
+	}
+}
