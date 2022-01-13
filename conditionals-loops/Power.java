02712ff7771ae360ae9530f26d6985a8dc4c4f0f
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-13 23:15:01 +0530

    Create Power.java

---
+++ conditionals-loops/Power.java
@@ -0,0 +1,11 @@
+//Power In Java
+import java.util.*;
+public class Power{  
+        public static void main(String[] args)     
+        {    
+            Scanner in = new Scanner(System.in);
+            double a = in.nextInt();    
+            double b = in.nextInt();    
+            System.out.println(Math.pow(a, b)); 
+        }    
+    }
