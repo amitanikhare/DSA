fd4e51f2ce3c637f22a34757638c005c3218675d
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-16 22:10:57 +0530

    Create BattingAverage.java

---
+++ conditionals-loops/Intermediate/BattingAverage.java
@@ -0,0 +1,18 @@
+//Calculate Batting Average
+import java.util.*;
+public class BattingAverage {
+    public static void main(String[] args){
+        // Calculate Batting Average
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of Runs: ");
+        int runs = input.nextInt();
+        System.out.println("Enter number of matches");
+        int matches=input.nextInt();
+        System.out.println("Enter number of not-out matches");
+        int notout=input.nextInt();
+        int outmatches=matches-notout;
+        double baverage=runs/outmatches;
+        System.out.println("Batting Average= "+baverage);
+
+    }
+}
