0c731d5fed18be38e497143f87e45bc4b952827e
Amita Nikhare <84002511+amitanikhare@users.noreply.github.com>
2022-01-29 00:16:31 +0530

    Create PalindromeOrNot.java

---
+++ Functions/Conditional-Loop/Intermidiate/PalindromeOrNot.java
@@ -0,0 +1,24 @@
+//Find if a number is palindrome or not
+import java.util.*;
+public class PalindromeOrNot{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter any number: ");
+		int num = in.nextInt();
+		
+		palindrome(num);
+	}
+	static void palindrome(int n){
+		int original =n ,rem =0, sum=0;
+		while(n>0){
+		    rem = n% 10;
+		    sum = (sum*10)+rem;
+		    n/=10;
+		}   
+		if(original == sum){
+		    System.out.println("Given Number is Palindrome");
+		}else{
+		    System.out.println("No it's not try again!");
+		} 
+	}
+}
